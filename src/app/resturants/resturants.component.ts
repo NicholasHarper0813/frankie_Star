907f034e1327c0befab3843c28ae55516ef6da56
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-10 08:08:51 -0800

    Update resturants.component.ts

--- src/app/resturants/resturants.component.ts
+++ src/app/resturants/resturants.component.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { SupabaseService } from '../supabase.service';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { CommonModule } from '@angular/common';
+import { SupabaseService } from '../supabase.service';
 
 @Component({
   selector: 'app-resturants',
   standalone: true,
-  imports: [CommonModule,ProgressSpinnerModule],
   templateUrl: './resturants.component.html',
-  styleUrl: './resturants.component.css'
+  styleUrl: './resturants.component.css',
+  imports: [CommonModule,ProgressSpinnerModule]
 })
 export class ResturantsComponent implements OnInit, OnDestroy {
   shopItems: any[] = [];
   loading: boolean = true;
   errorMessage: string = '';
-
   constructor(private supabaseService: SupabaseService) {}
   
   ngOnInit(): void {
@@ -22,17 +21,18 @@ export class ResturantsComponent implements OnInit, OnDestroy {
   }
   
   ngOnDestroy(): void {
-    // Cleanup code here
+    //this.destroy();
+    //all format
   }
   
   async fetchShopItems() {
     this.loading = true;
     try {
       this.shopItems = await this.supabaseService.getShopItems();
-    } catch (error: any) {
+    } 
+    catch (error: any) {
       this.errorMessage = 'Error fetching shop items: ' + error.message;
     }
     this.loading = false;
   }
-
 }
